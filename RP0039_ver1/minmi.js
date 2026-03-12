9ad56cfb92b7296597af9c55320864affed46473
RP0039 <RiveroVal3984@gmail.com>
2026-03-12 19:56:52 -0300

    me olvide eto

---
+++ RP0039_ver1/minmi.js
@@ -0,0 +1,46 @@
+
+// --- Control de ventanas estilo Windows 98 para 98.css ---
+document.querySelectorAll(".window").forEach(win => {
+  const body = win.querySelector(".window-body");
+  const mainBox = win.querySelector("#mainBox");
+
+  const btnMin = win.querySelector("button[aria-label='Minimize']");
+  const btnMax = win.querySelector("button[aria-label='Maximize']");
+  const btnClose = win.querySelector("button[aria-label='Close']");
+
+  let isMax = false;
+
+  // Guardar estado anterior
+  let prev = {
+    left: "",
+    top: "",
+    width: "",
+    height: "",
+    position: "",
+    marginTop: "",
+    mainBoxHeight: ""
+  };
+
+  // --- MINIMIZAR ---
+  btnMin.addEventListener("click", () => {
+    body.style.display =
+      body.style.display === "none" ? "" : "none";
+  });
+
+  // --- CERRAR ---
+  btnClose.addEventListener("click", () => {
+    win.style.display = "none";
+  });
+})
+
+    /*animacion* */
+const screen = document.getElementById("screen");
+const defaultImage = "./gif/proxy-image.gif";
+
+function changeScreen(image) {
+  screen.src = image;
+}
+
+function resetScreen() {
+  screen.src = defaultImage;
+}
